53fa66dcd3a0af22526f1a1e9aa88f4da912ad60
bedant <bedantchhetri7@gmail.com>
2025-07-31 23:18:02 +0530

    zod schema

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,11 +2,17 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
+import {CreateUserSchema,SigninSchema,CreateRoomSchema} from "@repo/common/types"
 
-const app = express()
-app.use(express())
+const app = express();
 
 app.post("/signin",(req,res)=>{
+    const data = SigninSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            "message":"Incorrect inputs"
+        })
+    }
     const username = req.body.username;
     const password = req.body.password;
     res.json({
@@ -15,6 +21,11 @@ app.post("/signin",(req,res)=>{
 })
 
 app.post("/signup",(req,res)=>{
+    const data = CreateUserSchema.safeParse(req.body);
+    if(!data.success){
+        res.status(403)
+        return
+    }
     const username = req.body.username;
     const password = req.body.password;
 
@@ -25,6 +36,13 @@ app.post("/signup",(req,res)=>{
 })
 
 app.post("/room",middleware,(req,res)=>{
+    const data = CreateRoomSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message:"invalid input"
+        })
+    }
+
     res.json({
         "message": "room"
     })
