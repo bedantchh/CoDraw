f72130348260f8323aeab5ae185996c5f51fe416
bedant <bedantchhetri7@gmail.com>
2025-08-01 18:01:38 +0530

    Added sign up endpoint

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,51 +2,65 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
-import {CreateUserSchema,SigninSchema,CreateRoomSchema} from "@repo/common/types"
+import {
+  CreateUserSchema,
+  SigninSchema,
+  CreateRoomSchema,
+} from "@repo/common/types";
+import prismaClient from "@repo/db/client";
 
 const app = express();
-
-app.post("/signin",(req,res)=>{
-    const data = SigninSchema.safeParse(req.body);
-    if(!data.success){
-        res.json({
-            "message":"Incorrect inputs"
-        })
-    }
-    const username = req.body.username;
-    const password = req.body.password;
+app.use(express.json());
+app.post("/signin", (req, res) => {
+  const parsedData = SigninSchema.safeParse(req.body);
+  if (!parsedData.success) {
     res.json({
-        "message": "signin"
+      message: "Incorrect inputs",
+    });
+  }
+  const username = req.body.username;
+  const password = req.body.password;
+  const token = jwt.sign({ username }, JWT_SECRET);
+  res.json({
+    message: "signin",
+  });
+});
+
+app.post("/signup", async (req, res) => {
+  const parsedData = CreateUserSchema.safeParse(req.body);
+  if (!parsedData.success) {
+    res.status(403);
+    return;
+  }
+  try{
+    await prismaClient.user.create({
+    data: {
+      email: parsedData.data?.username,
+      password: parsedData.data?.password,
+      name: parsedData.data?.name,
+    },
+  });
+  res.json({
+    message: "signin",
+  });
+  }catch(e){
+    res.status(411).json({
+        message: "could not sign up"
     })
-})
+  }
+});
 
-app.post("/signup",(req,res)=>{
-    const data = CreateUserSchema.safeParse(req.body);
-    if(!data.success){
-        res.status(403)
-        return
-    }
-    const username = req.body.username;
-    const password = req.body.password;
-
-    const token = jwt.sign({username},JWT_SECRET)
+app.post("/room", middleware, (req, res) => {
+  const data = CreateRoomSchema.safeParse(req.body);
+  if (!data.success) {
     res.json({
-        "message": "signin"
-    })
-})
+      message: "invalid input",
+    });
+  }
 
-app.post("/room",middleware,(req,res)=>{
-    const data = CreateRoomSchema.safeParse(req.body);
-    if(!data.success){
-        res.json({
-            message:"invalid input"
-        })
-    }
+  res.json({
+    message: "room",
+  });
+});
 
-    res.json({
-        "message": "room"
-    })
-})
-
-
-app.listen(3001)
+app.listen(3001);
--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const CreateUserSchema = z.object({
     username: z.string().min(3).max(20),
-    passwrod: z.string().min(6),
+    password: z.string().min(6),
     name: z.string()
 })
 
